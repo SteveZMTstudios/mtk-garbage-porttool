dc23d98688d66a8055132c10ac72587a26b1e458
ColdWindScholar <3590361911@qq.com>
2024-02-02 12:24:49 +0800

    [Tool] Fix Some Bugs

--- porttool/__main__.py
+++ porttool/__main__.py
@@ -4,15 +4,18 @@ from .ui import (
     MyUI
 )
 from os import name
+
 if name == 'nt':
     import ctypes
     from multiprocessing.dummy import freeze_support
+
     freeze_support()
+
 
 def main():
     root = Tk()
     root.title("MTK Port Tool")
-    #root.geometry("860x480")
+    # root.geometry("860x480")
 
     myapp = MyUI(root)
     myapp.pack(side='top', fill='both', padx=5, pady=5, expand='yes')
@@ -23,7 +26,7 @@ def main():
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
         # Get screen resize scale factor
         scalefactor = ctypes.windll.shcore.GetScaleFactorForDevice(0)
-        root.tk.call('tk', 'scaling', scalefactor/75)
+        root.tk.call('tk', 'scaling', scalefactor / 75)
 
     root.update()
     root.mainloop()
